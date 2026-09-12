f21b93c61adeb2036c34e1726806f6f47e96cfee
Mikaela Kristen C. De Guzman <mcdeguzman12@up.edu.ph>
2026-09-13 02:22:27 +0800

    fix: remove duplicate main method

--- Program.cs
+++ Program.cs
@@ -24,27 +24,9 @@ namespace Ck
             }
             else
             {
-                if (args.Length == 2 && args[0] == "--tokenize")
-                {
-                    return RunFile(args[1]);
-                }
-                else if (args.Length == 1)
-                {
-                    // Lab 0 legacy:  "./run <path>" with no flag
-                    Console.WriteLine("Hello, World!");
-                    return 0;
-                }
-                else if (args.Length == 0)
-                {
-                    RunRepl();
-                    return 0;
-                }
-                else
-                {
-                    return 64; //command line usage error
-                }
+                return 64; //command line usage error
             }
-
+        }
         private static int RunFile(string path)
         {
             string source;
